06edd3b462a0fd723a5221720f69d40f50b46913
rian <rianrgomes@gmail.com>
2024-02-28 14:11:28 -0300

    create register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm!: FormGroup
+  constructor(
+    private http: HttpService,
+    private fb: FormBuilder,
+    private router: Router
+  ){
+    this.registerForm = this.fb.group({
+      nick: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  };
 
+  createUser(){
+    const  { nick, password } = this.registerForm.value
+    this.http.createUser({
+      nick,
+      password
+    }).subscribe(data =>{
+      console.log(data)
+    })
+    this.registerForm.reset()
+
+    this.router.navigate(['/home'])
+  }
 }
